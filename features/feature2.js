889767cdc9ae3eed766f54d26c0a8fe936f5a8f9
EMOLLP4 <emolero@profile.es>
2022-12-26 09:40:43 +0100

    added feature 2

---
+++ features/feature2.js
@@ -0,0 +1,10 @@
+const Initialize = () =>{
+    return "Initialize Feature 2..."
+}
+const Feature2Print = () =>{
+    return "Print Feature 2"
+}
+module.exports = {
+    "Initialize":Initialize(),
+    "Feature2Print":Feature2Print()
+}
