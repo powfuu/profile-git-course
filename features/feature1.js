bc338a249469e778658474674e435b58f84a2115
EMOLLP4 <emolero@profile.es>
2022-12-26 09:37:50 +0100

    Added feature 1 to profile project git

---
+++ features/feature1.js
@@ -0,0 +1,10 @@
+const Initialize = () =>{
+    return "Initialize Feature 1"
+}
+const Feature1Print = () =>{
+    return "Print Feature 1"
+}
+module.exports = {
+    "Initialize":Initialize(),
+    "Feature1Print":Feature1Print()
+}
--- main.js
+++ main.js
@@ -1,1 +1,4 @@
+const f1 = require('./features/feature1')
 console.log("Initialized Main Function Profile Git Course")
+console.log(f1.Initialize)
+console.log(f1.Feature1Print)
